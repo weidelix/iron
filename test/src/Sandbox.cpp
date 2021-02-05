1db6fcccb108c2502d2c093272fb893318286af7
weidelix <dev.mesina@gmail.com>
2021-02-05 16:18:44 +0800

    Refactoring of file

--- test/src/Sandbox.cpp
+++ test/src/Sandbox.cpp
@@ -6,12 +6,12 @@ using namespace Iron;
 class Sandbox : public Application
 {
 public:
-	GameObject plane;
+	GameObject plane = GameObject::CreatePrimitive(Primitives::Plane);
 	
 	// Runs code on start
 	void Start() override 
 	{
-		plane = GameObject::CreatePrimitive(Primitives::Plane);
+
 	}
 
 	// Runs code every frame
